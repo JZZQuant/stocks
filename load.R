28570483823887f9065076c47316f0808c1816e7
sasikanth <v00d00childblues1@gmail.com>
2015-06-29 21:07:12 +0530

    Function to load data in batches

--- load.R
+++ load.R
@@ -8,14 +8,26 @@ mydb = dbConnect(MySQL(), user=config$db$user, password=config$db$password, dbna
 if(length(mydb) <1){stop("Database connection could not be made")}
 setwd(config$ftp$downloadDir)
 
+#https://github.com/rstats-db/RMySQL/blob/master/R/table.R#L104 bulk inserts all records instead of inserting in a loop
+# so we just need to set the auto increment to 2 (this has to be done before executing the script)
+
 # write data to mysql table
-x<-dbWriteTable(mydb, name='options_eod_table',row.names = FALSE, value=df.options,overwrite = FALSE, append = TRUE,header = F)
-if(!x){stop("options_eod_table Data could not be written correctly")}
-y<-dbWriteTable(mydb, name='optionstats_eod_table',row.names = FALSE, value=df.stats,overwrite = FALSE, append = TRUE,header = F)
-if(!y){stop("optionstats_eod_table Data could not be written correctly")}
-z<-dbWriteTable(mydb, name='stockquotes_eod_table',row.names = FALSE, value=df.stocks,overwrite = FALSE, append = TRUE,header = F)
-if(!z){stop("stockquotes_eod_table Data could not be written correctly")}
+batchwritetable(mydb,'options_eod_table',df.options,1000)
+batchwritetable(mydb,'optionstats_eod_table',df.stats,1000)
+batchwritetable(mydb,'stockquotes_eod_table',df.stocks,1000)
 
 dbDisconnect(mydb)
 
 setwd('..')
+
+
+batchwritetable<-function(db,table,df,size)
+{
+  df.batches<-split(dfrm, (0:nrow(df) %/% size))
+  apply(df.batches,1,function(batch) 
+    {  
+  x<-dbWriteTable(db, name=table,row.names = FALSE, value=batch,overwrite = FALSE, append = TRUE,header = F)
+  if(!x){stop("table Data could not be written correctly")}
+    } 
+       )
+}
